5485627217988ad031e21eb1d4a1803ac05bacfe
Hariindium <harihara.s@indiumsoft.com>
2020-03-20 20:34:15 +0530

    added

---
+++ SampleViewController.h
@@ -0,0 +1,17 @@
+//
+//  SampleViewController.h
+//  Maps
+//
+//  Created by HariharasSudhan on 20/03/20.
+//  Copyright © 2020 Indium. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface SampleViewController : UIViewController
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ SampleViewController.m
@@ -0,0 +1,32 @@
+//
+//  SampleViewController.m
+//  Maps
+//
+//  Created by HariharasSudhan on 20/03/20.
+//  Copyright © 2020 Indium. All rights reserved.
+//
+
+#import "SampleViewController.h"
+
+@interface SampleViewController ()
+
+@end
+
+@implementation SampleViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
